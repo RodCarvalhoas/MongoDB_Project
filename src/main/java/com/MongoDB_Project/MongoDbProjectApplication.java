9b6916fd3557eefa8621580c6b0d35711dd8ccaa
RodCarvalhoas <sutil_rodrigo@hotmail.com>
2023-01-16 22:03:21 -0300

    Projeto criado

---
+++ src/main/java/com/MongoDB_Project/MongoDbProjectApplication.java
@@ -0,0 +1,13 @@
+package com.MongoDB_Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoDbProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongoDbProjectApplication.class, args);
+	}
+
+}
