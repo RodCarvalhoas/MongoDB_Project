15da662659e45449dcbf73ef079055ff62748b4c
RodCarvalhoas <sutil_rodrigo@hotmail.com>
2023-01-23 22:53:26 -0300

    AuthotDTO added

--- src/main/java/com/MongoDB_Project/config/configuration.java
+++ src/main/java/com/MongoDB_Project/config/configuration.java
@@ -2,13 +2,13 @@ package com.MongoDB_Project.config;
 
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
-import java.util.Date;
 import java.util.TimeZone;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.MongoDB_Project.dto.AuthorDTO;
 import com.MongoDB_Project.model.Post;
 import com.MongoDB_Project.model.User;
 import com.MongoDB_Project.repositories.PostRepository;
@@ -39,8 +39,8 @@ public class configuration implements CommandLineRunner{
 		
 		userRepository.saveAll(Arrays.asList(maria,alex,bob));
 		
-		Post post1 = new Post(null, sdf.parse("23/10/2002"), "Partiu viagem", "Vou viajar para Pernambuco", maria);
-		Post post2 = new Post(null, sdf.parse("21/09/2002"), "Bom dia", "Acordei Feliz Hoje!", maria);
+		Post post1 = new Post(null, sdf.parse("23/10/2002"), "Partiu viagem", "Vou viajar para Pernambuco", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("21/09/2002"), "Bom dia", "Acordei Feliz Hoje!", new AuthorDTO(maria));
 		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
